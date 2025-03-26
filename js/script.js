acdfa334e3f543bd23c1bdc1a4df3121ac436c38
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-26 16:28:39 +0100

    [UPD] aggiunte classi bootstrap

--- js/script.js
+++ js/script.js
@@ -8,21 +8,22 @@ const age = document.getElementById('age')
 const button = document.querySelector('.send')
 
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
 
-let distancePrice = (distance * 0.21).toFixed(2);
-console.log(distancePrice + " " + "Euro (Prezzo intero)");
+    let distancePrice = (distance.value * 0.21).toFixed(2);
+    console.log(distancePrice + " " + "Euro (Prezzo intero)");
 
-if(age < 18){
-    let priceUnderage = (distancePrice - distancePrice * 20 / 100).toFixed(2);
-    console.log(priceUnderage  + " " + "Euro (prezzo scontato per minorenni)");
-}
+    if (age.value < 18) {
+        let priceUnderage = (distancePrice - distancePrice * 20 / 100).toFixed(2);
+        console.log(priceUnderage + " " + "Euro (prezzo scontato per minorenni)");
+    }
 
-else if(age > 65){
-    let priceOver65 = (distancePrice - distancePrice * 40 / 100).toFixed(2);
-    console.log(priceOver65  + " " + "Euro (Prezzo scontato per maggiorenni)");
-}
+    else if (age.value > 65) {
+        let priceOver65 = (distancePrice - distancePrice * 40 / 100).toFixed(2);
+        console.log(priceOver65 + " " + "Euro (Prezzo scontato per maggiorenni)");
+    }
 
-else{
-    console.log(distancePrice + " " + "Euro (Prezzo intero, non rientri in una fascia di età in sconto)")}
+    else {
+        console.log(distancePrice + " " + "Euro (Prezzo intero, non rientri in una fascia di età in sconto)")
+    }
 })
